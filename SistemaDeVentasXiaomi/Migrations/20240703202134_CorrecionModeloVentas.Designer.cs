a111e1facbd5a4b187d91fa6ecce016657261d08
Ittan Luna Sanchez <112011386+IttanLuna@users.noreply.github.com>
2024-07-03 16:22:06 -0400

    cambio en el modelo venta

--- SistemaDeVentasXiaomi/Migrations/20240703202134_CorrecionModeloVentas.Designer.cs
+++ SistemaDeVentasXiaomi/Migrations/20240703202134_CorrecionModeloVentas.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaDeVentasXiaomi.Contexto;
 
@@ -10,9 +11,11 @@ using SistemaDeVentasXiaomi.Contexto;
 namespace SistemaDeVentasXiaomi.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20240703202134_CorrecionModeloVentas")]
+    partial class CorrecionModeloVentas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
--- SistemaDeVentasXiaomi/Models/Venta.cs
+++ SistemaDeVentasXiaomi/Models/Venta.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SistemaDeVentasXiaomi.Models
 {
@@ -6,15 +7,25 @@ namespace SistemaDeVentasXiaomi.Models
     {
         [Key]
         public int VentaId { get; set; }
-        public int ClienteId { get; set; }
-        public Cliente Cliente { get; set; }
-        public int VendedorId { get; set; }
-        public Vendedor Vendedor { get; set; }
-        public int TelevisorId { get; set; }
-        public Televisor Televisor { get; set; }
-        public DateTime FechaVenta { get; set; }
+        public int NroVenta {  get; set; }
+        [Column(TypeName = "date")]
+        public DateOnly FechaVenta { get; set; }
+        [Required]
+        public int Mes {  get; set; }
+        [Required]
+        public int Anio { get; set; }
+        [Required]
+        public string? Detalle { get; set; }
         public int Cantidad { get; set; }
+        public decimal PrecioUnidad { get; set; }
         public decimal Total { get; set; }
+
+        //Relaciones
+
+        public int ClienteId { get; set; }
+        public int TelevisorId { get; set; }
+        public Cliente? Cliente { get; set; }
+        public Televisor? Televisor { get; set; }
     }
 
 }
